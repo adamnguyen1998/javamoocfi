827038af601f1c6d22f8de3e58b4d61680cc747f
adamnguyen1998 <83429705+adamnguyen1998@users.noreply.github.com>
2024-01-09 12:16:14 -0500

    Add files via upload

--- Part 6 - MessagingService/MessagingService.java
+++ Part 6 - MessagingService/MessagingService.java
@@ -1,1 +1,26 @@
+import java.util.ArrayList;
 
+public class MessagingService{
+    
+    private ArrayList<Message> message;
+    
+    
+    public MessagingService(){
+        
+        this.message = new ArrayList<>();
+        
+    }
+    
+    public void add(Message message){
+        
+        if (message.getContent().length() <= 280) //cannot use this.message because message is the parameter given
+        this.message.add(message);
+    }
+    
+    
+    
+    public ArrayList<Message> getMessages(){
+        return this.message;
+    }
+    
+}

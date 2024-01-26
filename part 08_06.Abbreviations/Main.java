39edc968151d5fd2ff52a5b3b23c1d6bb28a7792
adamnguyen1998 <83429705+adamnguyen1998@users.noreply.github.com>
2024-01-26 14:18:07 -0500

    Create Main.java

---
+++ part 08_06.Abbreviations/Main.java
@@ -0,0 +1,30 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+public class Main
+{
+	public static void main(String[] args) {
+		Abbreviations abbreviations = new Abbreviations();
+abbreviations.addAbbreviation("e.g.", "for example");
+abbreviations.addAbbreviation("etc.", "and so on");
+abbreviations.addAbbreviation("i.e.", "more precisely");
+
+String text = "e.g. i.e. etc. lol";
+
+for (String part: text.split(" ")) {
+    if(abbreviations.hasAbbreviation(part)) {
+        part = abbreviations.findExplanationFor(part);
+    }
+
+    System.out.print(part);
+    System.out.print(" ");
+}
+
+System.out.println();
+	}
+}
